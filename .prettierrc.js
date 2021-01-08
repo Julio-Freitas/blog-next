23e24b67a24f8048cc4a2191913e4b99c0206582
julio-freitas <julio.clf31@gmail.com>
2021-01-08 08:42:17 -0300

    NEW: mount system folder

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  pintWidth: 80,
+  tabWidth: 2,
+};
---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
