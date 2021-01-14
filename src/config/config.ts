e7d0f86e926e45273e47bc910d0db7c3bfc56e1c
julio-freitas <julio.clf31@gmail.com>
2021-01-14 08:45:40 -0300

    NEW: add site url

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://fierce-sands-23212.herokuapp.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = process.env.customKey || 'Não pegou';
-export const SITE_URL = '';
+export const SITE_URL = 'https://nervous-bose-42bf21.netlify.app';
